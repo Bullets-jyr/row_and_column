84807e37e51d2c0eec681fe43a4bdb6eea5fc2bf
Bullets-jyr <jyr@bullets.co.kr>
2023-04-12 15:44:24 +0900

    세팅하기

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Container 위젯의 형태는 원하는대로 지정을 할 수 있지만,
+      // 일반적으로 child 위젯의 형태를 따라갑니다.
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Container(
+                color: Colors.red,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.orange,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.green,
+                width: 50.0,
+                height: 50.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
