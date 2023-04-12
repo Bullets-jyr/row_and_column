0496a48728a8fb70b0fb86448185d0c21ca63963
Bullets-jyr <jyr@bullets.co.kr>
2023-04-12 17:31:23 +0900

    CrossAxisAlignment 완전정복

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,9 +12,15 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
+          // 실행 중인 앱의 사이즈: MediaQuery.of(context).size
+          // width: MediaQuery.of(context).size.width: Column 테스트
+          // height: MediaQuery.of(context).size.height: Row 테스트
+          height: MediaQuery.of(context).size.height,
           child: Row(
+            // MainAxisAlignment, CrossAxisAlignment 동시 적용 가능
+
             // Column일 경우,
-            // MainAxisAlignment - 주축(세로) 정렬
+            // MainAxisAlignment - 주축(세로) 정렬, 차지할 수 있는 남는 공간의 최대 크기를 차지한다.
             // start - 시작 (최상단)
             // end - 끝 (최하단)
             // center - 가운데
@@ -23,7 +29,7 @@ class HomeScreen extends StatelessWidget {
             // spaceAround - spaceEvenly + 끝과 끝의 간격은 1/2 (spaceEvenly 끝 빈 간격 기준 1/2)
 
             // Row일 경우,
-            // MainAxisAlignment - 주축(가로) 정렬
+            // MainAxisAlignment - 주축(가로) 정렬, 차지할 수 있는 남는 공간의 최대 크기를 차지한다.
             // start - 시작 (최좌측)
             // end - 끝 (최우측)
             // center - 가운데
@@ -31,6 +37,14 @@ class HomeScreen extends StatelessWidget {
             // spaceEvenly - 위젯을 같은 간격으로 배치하지만 끝과 끝에도 위젯이 아닌 빈 간격으로 시작한다.
             // spaceAround - spaceEvenly + 끝과 끝의 간격은 1/2 (spaceEvenly 끝 빈 간격 기준 1/2)
             mainAxisAlignment: MainAxisAlignment.start,
+
+            // 공통
+            // CrossAxisAlignment - 반대축 정렬 (Column일 경우 가로, Row일 경우 세로)이며, 최소한의 사이즈만을 차지한다. 즉 child 위젯들의 사이즈만큼만 차지한다.
+            // start - 시작
+            // end - 끝
+            // center - 가운데 (CrossAxisAlignment 설정하지 않아도 세팅되는 Default 값)
+            // stretch - 최대한으로 늘린다. (강제로 하위 위젯들의 사이즈가 늘어난다.)
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 color: Colors.red,
